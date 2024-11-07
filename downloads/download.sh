ded830dd86a26d66ca6e5e3b97df066c8ecca44c
cpegeric <cpegeric@gmail.com>
2024-11-07 14:43:55 +0000

    get index.html to get the download list

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,11 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
-PWD=`pwd`
+mkdir -p zh en
 
-# make download directory
-mkdir zh en
+if ! wget https://dumps.wikimedia.org/enwiki/latest/ ; then
+	echo "wget failed"
+	exit 1
+fi
 
-# download the files
-(cd zh ; bash ../zhwiki.sh)
-(cd en ; bash ../enwiki.sh)
+#xml
+egrep -e "wiki.*.stream[0-9].*.xml.*.bz2\"" index.html | sed -e "s/<a href=\"\(.*\)\".*/\1/g" |  \
+	awk '{print "wget https://dumps.wikimedia.org/enwiki/latest/" $0}' > download_en.sh
 
+#index
+egrep -e "wiki.*.stream-index[0-9].*.txt.*.bz2\"" index.html | sed -e "s/<a href=\"\(.*\)\".*/\1/g" | \
+       	awk '{print "wget https://dumps.wikimedia.org/enwiki/latest/" $0}' >> download_en.sh
+
+rm index.html
+
+
+if ! wget https://dumps.wikimedia.org/zhwiki/latest/ ; then
+        echo "wget failed"
+        exit 1
+fi
+
+#xml
+egrep -e "wiki.*.stream[0-9].*.xml.*.bz2\"" index.html | sed -e "s/<a href=\"\(.*\)\".*/\1/g" |  \
+        awk '{print "wget https://dumps.wikimedia.org/zhwiki/latest/" $0}' > download_zh.sh
+
+#index
+egrep -e "wiki.*.stream-index[0-9].*.txt.*.bz2\"" index.html | sed -e "s/<a href=\"\(.*\)\".*/\1/g" | \
+        awk '{print "wget https://dumps.wikimedia.org/zhwiki/latest/" $0}' >> download_zh.sh
+
+rm index.html
+
+
+#(cd zh ; bash ../download_zh.sh)
+#(cd en ; bash ../download_en.sh)

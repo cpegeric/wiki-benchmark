18509f4ccb4bb10dd398c221463280a7f47db8dc
cpegeric <cpegeric@gmail.com>
2024-12-04 14:21:24 +0000

    process whole csv

--- python/wiki2text.py
+++ python/wiki2text.py
@@ -1,6 +1,7 @@
 import wikitextparser as wtp
 import os
 import sys
+import csv
 
 # installation
 # pip install wikitextparser
@@ -20,6 +21,18 @@ def wiki2txt(wikitext):
 
 if __name__ == "__main__":
 
-    wiki = sys.stdin.read()
-    text = wiki2txt(wiki)
-    print(text, end="")
+    if len(sys.argv) != 3:
+        print("usage: python wiki2text.py infile outfile")
+        sys.exit(1)
+
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    csv.field_size_limit(sys.maxsize)
+    with open(infile) as csvfile:
+        r = csv.reader(csvfile)
+        with open(outfile, 'w') as wfile:
+            writer = csv.writer(wfile)
+            for row in r:
+                txt = wiki2txt(row[2])
+                writer.writerow((row[0], row[1], txt))
